abdb62dba5f84bd2ecee702ab59af953c3ccab44
Minhhieu203 <thaiminhhieu2503@gmail.com>
2026-05-16 00:59:43 +0700

    Force resolve react-is in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react-is': 'react-is',
+    },
+  },
+  optimizeDeps: {
+    include: ['recharts', 'react-is'],
+  }
 })
